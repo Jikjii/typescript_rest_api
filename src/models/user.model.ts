c0d1d7e2abc931cbb162cf7835de8ed222a151e8
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-06-23 16:29:30 -0400

    create user implemented

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -9,6 +9,7 @@ export interface UserDocument extends mongoose.Document {
   password: string;
   createdAt: Date;
   updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<boolean>
 }
 
 // Schema definition
@@ -22,6 +23,31 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  let user = this as UserDocument;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  const user = this as UserDocument;
+
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
 // Model
 const UserModel = mongoose.model("User", userSchema);
 
