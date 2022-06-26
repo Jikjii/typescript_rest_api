1ecb49285de2155a30b9f1a6509ed8e1fd029630
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-06-26 16:19:10 -0400

    Updated Config *bug fix*
    Error: error:0908F066:PEM routines:get_header_and_data:bad end line - error will occur if RSA keys are not formatted properly

--- config/default.ts
+++ config/default.ts
@@ -1,29 +1,29 @@
 export default {
-  port: 1337,
-  dbUri:
-    "mongodb+srv://jikjii:jikjii@cluster0.w5xqycr.mongodb.net/?retryWrites=true&w=majority",
-  saltWorkFactor: 10,
-  accessTokenTtl: '15m',
-  refreshToken: '1y',
-  publicKey: `-----BEGIN PUBLIC KEY-----
-  MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgFL3SaL5lp1jb+DXtFN8mdWVAutH
-  2iXzBh0O3Tk5ECOEiChLEVWaVh7/thZamWoB/cyRiLyzl+khshJnW7GRg4V68eM5
-  MaoswXvTOrm7RAfS+rWKKBtYHbfva6w3v+Z56WZujkbK7n8wsFQxbCDa+1lxcTnS
-  zJgSD9MCJAeNos1LAgMBAAE=
-  -----END PUBLIC KEY-----`,
-  privateKey: `-----BEGIN RSA PRIVATE KEY-----
-  MIICWwIBAAKBgFL3SaL5lp1jb+DXtFN8mdWVAutH2iXzBh0O3Tk5ECOEiChLEVWa
-  Vh7/thZamWoB/cyRiLyzl+khshJnW7GRg4V68eM5MaoswXvTOrm7RAfS+rWKKBtY
-  Hbfva6w3v+Z56WZujkbK7n8wsFQxbCDa+1lxcTnSzJgSD9MCJAeNos1LAgMBAAEC
-  gYAfFShQV6O22W8r1IRpls/ndck/Lo8rEo5iypPVAWzrcZa1SdJ3uLe1K2Yjn9PE
-  JHTadFe/IqWyIP6e6Hww3WxhYz2Z5YL04PKL/wNKJL5ahTXb8SrnENCbbSxwk3iW
-  8w/VDWGA2xrFBSgZue4rbcyRQq9ILwafMOmcys6UdKpGeQJBAJzCr2wet0ToPlq1
-  eQ1AYsq62AXKoRRm3KjczLQ9qTMPeIrDR44XOR4Smr7n2uTvy7ajUSztLCae42+8
-  /ecWJcUCQQCHfSDFjDQ/woAsV5Km5fKL+791F5DkjkOSzCmIJZNJuIhANP1Vbink
-  D5UJthub+1GuaX5GO7FyrIaiz09D6GfPAkB6KdpNlw5Z2+ekByKEKjYTKVLqCIRJ
-  gvPZgODiYd83FIprEU31NLHldrgzqDpVCW8nMNU0/svWeSJjVNRFv2PFAkBWjE/K
-  4Wze7dgCe05q3qi5Uu9SVONxnI79zv0Ywyf7B63Blhfd4qlPzcRel727PidHSown
-  QSpxHlRC3Dq+Hk9xAkEAm/F2Vxvu3tH9VGy+j3javUBkQu3uGj8mT6gw8oeKB6LP
-  nZc74IMrlDaDO3S3tFaVr3GAmvjHYywPI6jrFPtQ5Q==
-  -----END RSA PRIVATE KEY-----`,
+port: 1337,
+dbUri:
+"mongodb+srv://jikjii:jikjii@cluster0.w5xqycr.mongodb.net/?retryWrites=true&w=majority",
+saltWorkFactor: 10,
+accessTokenTtl: "15m",
+refreshToken: "1y",
+publicKey: `-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgFL3SaL5lp1jb+DXtFN8mdWVAutH
+2iXzBh0O3Tk5ECOEiChLEVWaVh7/thZamWoB/cyRiLyzl+khshJnW7GRg4V68eM5
+MaoswXvTOrm7RAfS+rWKKBtYHbfva6w3v+Z56WZujkbK7n8wsFQxbCDa+1lxcTnS
+zJgSD9MCJAeNos1LAgMBAAE=
+-----END PUBLIC KEY-----`,
+privateKey: `-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgFL3SaL5lp1jb+DXtFN8mdWVAutH2iXzBh0O3Tk5ECOEiChLEVWa
+Vh7/thZamWoB/cyRiLyzl+khshJnW7GRg4V68eM5MaoswXvTOrm7RAfS+rWKKBtY
+Hbfva6w3v+Z56WZujkbK7n8wsFQxbCDa+1lxcTnSzJgSD9MCJAeNos1LAgMBAAEC
+gYAfFShQV6O22W8r1IRpls/ndck/Lo8rEo5iypPVAWzrcZa1SdJ3uLe1K2Yjn9PE
+JHTadFe/IqWyIP6e6Hww3WxhYz2Z5YL04PKL/wNKJL5ahTXb8SrnENCbbSxwk3iW
+8w/VDWGA2xrFBSgZue4rbcyRQq9ILwafMOmcys6UdKpGeQJBAJzCr2wet0ToPlq1
+eQ1AYsq62AXKoRRm3KjczLQ9qTMPeIrDR44XOR4Smr7n2uTvy7ajUSztLCae42+8
+/ecWJcUCQQCHfSDFjDQ/woAsV5Km5fKL+791F5DkjkOSzCmIJZNJuIhANP1Vbink
+D5UJthub+1GuaX5GO7FyrIaiz09D6GfPAkB6KdpNlw5Z2+ekByKEKjYTKVLqCIRJ
+gvPZgODiYd83FIprEU31NLHldrgzqDpVCW8nMNU0/svWeSJjVNRFv2PFAkBWjE/K
+4Wze7dgCe05q3qi5Uu9SVONxnI79zv0Ywyf7B63Blhfd4qlPzcRel727PidHSown
+QSpxHlRC3Dq+Hk9xAkEAm/F2Vxvu3tH9VGy+j3javUBkQu3uGj8mT6gw8oeKB6LP
+nZc74IMrlDaDO3S3tFaVr3GAmvjHYywPI6jrFPtQ5Q==
+-----END RSA PRIVATE KEY-----`,
 };
